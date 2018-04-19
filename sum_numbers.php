44fe849b1a4cd003f18daf8216e6d4df78a56b1e
Razvan <norses.razvan@gmail.com>
2018-04-19 13:24:09 +0300

    version 2

--- sum_numbers.php
+++ sum_numbers.php
@@ -1,9 +1,9 @@
 <?php
 function sumBy($array, $divideBy) {
     $sum=0;
-    foreach ($array as $valuesss) {
-        if ($valuesss % $divideBy == 0) {
-            $sum+=$valuesss;
+    foreach ($array as $value) {
+        if ($value % $divideBy == 0) {
+            $sum+=$value;
         }
     }echo $sum;
 }
